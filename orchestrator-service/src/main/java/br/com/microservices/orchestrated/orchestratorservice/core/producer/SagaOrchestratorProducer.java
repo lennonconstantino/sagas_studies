b89c435aca0691b8ca483ba642c73cc174fa972d
lennonconstantino <lennonconstantino@gmail.com>
2025-11-05 21:31:30 -0300

    Create producers

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/producer/SagaOrchestratorProducer.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/producer/SagaOrchestratorProducer.java
@@ -1,27 +1,24 @@
 package br.com.microservices.orchestrated.orchestratorservice.core.producer;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
-public class KafkaProducer {
+@AllArgsConstructor
+public class SagaOrchestratorProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @Value("${spring.kafka.topic.orchestrator}")
-    private String orchestratorTopic;
-
-    public void sendEvent(String payload) {
+    public void sendEvent(String payload, String topic) {
         try {
-            log.info("Sending event to topic {} with data {}", orchestratorTopic, payload);
-            kafkaTemplate.send(orchestratorTopic, payload);
+            log.info("Sending event to topic {} with data {}", topic, payload);
+            kafkaTemplate.send(topic, payload);
         } catch (Exception ex) {
-            log.error("Error trying to send data to topic {} with data {}", orchestratorTopic, payload, ex);
+            log.error("Error trying to send data to topic {} with data {}", topic, payload, ex);
         }
     }
 }
